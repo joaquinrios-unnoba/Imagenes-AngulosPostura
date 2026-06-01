0bfaa576e2ae4f2a7627c32ebb10c25bdaddb791
Joaquin Rios <jrios912@comunidad.unnoba.edu.ar>
2026-06-01 18:50:28 -0300

    Postura cambio para mostrar angulos especificos, si queres la version anterior buscala en el historial, esto con el fin de poder trabajar tranquilo con git, y mas adelante permitir seleccionar que angulos desea ver

--- Postura.py
+++ Postura.py
@@ -10,14 +10,23 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-def dibujar_angulos(image, angulos):
+#def dibujar_angulos(image, angulos):
+#    for i, (nombre, ang) in enumerate(angulos.items()):
+#        cv2.putText(image, f"{nombre}: {int(ang)}",
+#            (10, 40 + i*35),          # separo un poco más las líneas
+#            cv2.FONT_HERSHEY_SIMPLEX,
+#            1.0,                      # tamaño más grande
+#            (0, 0, 255),              # rojo (BGR)
+#            2)                        # más grosor
+def dibujar_angulos(image, angulos):                #Solo codo_derecho
     for i, (nombre, ang) in enumerate(angulos.items()):
-        cv2.putText(image, f"{nombre}: {int(ang)}",
-            (10, 40 + i*35),          # separo un poco más las líneas
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1.0,                      # tamaño más grande
-            (0, 0, 255),              # rojo (BGR)
-            2)                        # más grosor
+        if (nombre == "codo_der") or (nombre == "hombro_der"):
+            cv2.putText(image, f"{nombre}: {int(ang)} grados",
+                (10, 40 + i*35),          # separo un poco más las líneas
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1.2,                      # tamaño más grande
+                (0, 0, 0),              # rojo (BGR)
+                3)                        # más grosor
 
 def dibujar_esqueleto(image, landmarks):
     mp_drawing.draw_landmarks(
@@ -26,9 +35,138 @@ def dibujar_esqueleto(image, landmarks):
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
     )
+   
+anguloMaxCodoD = 0
+anguloMinCodoD = 180  
+anguloMaxHombroD = 0
+anguloMinHombroD = 180    
+#def mostrar_panel_angulo(angulo):
+#    global anguloMaxCodoD, anguloMinCodoD
+#    if angulo > anguloMaxCodoD:
+#        anguloMaxCodoD = angulo
+#    if angulo < anguloMinCodoD:
+#        anguloMinCodoD = angulo
 
+#    panel = np.zeros((400, 400, 3), dtype=np.uint8)
+
+    # Texto principal
+#    cv2.putText(panel,
+#        f"{int(angulo)} grados",
+#        (70, 85),
+#        cv2.FONT_HERSHEY_SIMPLEX,
+#        1.5,
+#        (0,255,0),
+#        3)
+#    # Max y Min abajo del todo
+#    cv2.putText(panel,
+#        f"Maximo: {int(anguloMaxCodoD)}",
+#        (70, 340),
+#        cv2.FONT_HERSHEY_SIMPLEX,
+#        0.9,
+#        (0,255,255),
+#        2)
+
+#    cv2.putText(panel,
+#        f"Minimo: {int(anguloMinCodoD)}",
+#        (70, 380),
+#        cv2.FONT_HERSHEY_SIMPLEX,
+#        0.9,
+#        (255,255,0),
+#        2)
+
+    # círculo base
+#    cv2.circle(panel, (200,200), 100, (255,255,255), 2)
+
+    # línea fija
+ #   cv2.line(panel, (200,200), (300,200), (255,255,255), 3)
+
+    # línea móvil según ángulo
+#    x = int(200 + 100*np.cos(np.radians(angulo)))
+#    y = int(200 - 100*np.sin(np.radians(angulo)))
+
+    #cv2.line(panel, (200,200), (x,y), (0,255,0), 4)
+
+#    cv2.imshow("Angulo Codo Derecho", panel)     
+
+def mostrar_panel_angulo(anguloCodo, anguloHombro):
+    global anguloMaxCodoD, anguloMinCodoD, anguloMaxHombroD, anguloMinHombroD
+
+    # ---- Actualizar máximos y mínimos ----
+    if anguloCodo > anguloMaxCodoD:
+        anguloMaxCodoD = anguloCodo
+    if anguloCodo < anguloMinCodoD:
+        anguloMinCodoD = anguloCodo
+
+    if anguloHombro > anguloMaxHombroD:
+        anguloMaxHombroD = anguloHombro
+    if anguloHombro < anguloMinHombroD:
+        anguloMinHombroD = anguloHombro
+
+    # Panel más grande
+    panel = np.zeros((400, 500, 3), dtype=np.uint8)
+
+    # ======================================================
+    # ================== CODO DERECHO =======================
+    # ======================================================
+
+    # Textos codo
+    cv2.putText(panel, f"Codo: {int(anguloCodo)}", (20, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+
+    cv2.putText(panel, f"Max:{int(anguloMaxCodoD)}", (20, 55),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,255), 1)
+
+    cv2.putText(panel, f"Min:{int(anguloMinCodoD)}", (20, 75),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,0), 1)
+
+    # Centro
+    centroCodo = (120, 200)
+
+    cv2.circle(panel, centroCodo, 100, (255,255,255), 2)
+
+    cv2.line(panel,
+        centroCodo,
+        (centroCodo[0] + 100, centroCodo[1]),
+        (255,255,255),
+        3)
+
+    x1 = int(centroCodo[0] + 100*np.cos(np.radians(anguloCodo)))
+    y1 = int(centroCodo[1] - 100*np.sin(np.radians(anguloCodo)))
+
+    cv2.line(panel, centroCodo, (x1,y1), (0,255,0), 4)
+
+    # ======================================================
+    # ================= HOMBRO DERECHO =====================
+    # ======================================================
+
+    cv2.putText(panel, f"Hombro: {int(anguloHombro)}", (270, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+
+    cv2.putText(panel, f"Max:{int(anguloMaxHombroD)}", (270, 55),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,255), 1)
+
+    cv2.putText(panel, f"Min:{int(anguloMinHombroD)}", (270, 75),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,0), 1)
+
+    centroHombro = (380, 200)
+
+    cv2.circle(panel, centroHombro, 100, (255,255,255), 2)
+
+    cv2.line(panel,
+        centroHombro,
+        (centroHombro[0] + 100, centroHombro[1]),
+        (255,255,255),
+        3)
+
+    x2 = int(centroHombro[0] + 100*np.cos(np.radians(anguloHombro)))
+    y2 = int(centroHombro[1] - 100*np.sin(np.radians(anguloHombro)))
+
+    cv2.line(panel, centroHombro, (x2,y2), (0,255,0), 4)
+
+    cv2.imshow("Angulos", panel)
+    
 def procesar_video(video):
-
+    global anguloMaxCodoD, anguloMinCodoD, anguloMaxHombroD, anguloMinHombroD
     cap = cv2.VideoCapture(video)
 
     cv2.namedWindow('Pose Detection', cv2.WINDOW_NORMAL)
@@ -64,12 +202,23 @@ def procesar_video(video):
 
                 # 🔹 calcular ángulos
                 angulos = calcular_angulos_cuerpo(landmarks, w, h)
+                
+                if angulos["codo_der"] > anguloMaxCodoD:     #Para guardar el angulo max del codo derecho
+                    anguloMaxCodoD = angulos["codo_der"]
+                if angulos["codo_der"] < anguloMinCodoD:     #Para guardar el angulo min del codo derecho
+                    anguloMinCodoD = angulos["codo_der"]
+                if angulos["hombro_der"] > anguloMaxHombroD:
+                    anguloMaxHombroD = angulos["hombro_der"]
+                if angulos["hombro_der"] < anguloMinHombroD:
+                    anguloMinHombroD = angulos["hombro_der"]
 
                 # 🔹 dibujar ángulos
                 dibujar_angulos(image, angulos)
 
                 # 🔹 dibujar esqueleto
                 dibujar_esqueleto(image, results.pose_landmarks)
+                
+                mostrar_panel_angulo(angulos["codo_der"], angulos["hombro_der"])
 
             cv2.imshow('Pose Detection', image)
 
---
+++ README.md
@@ -0,0 +1,83 @@
+# Análisis de Postura con Python
+
+Este proyecto permite analizar la postura de una persona utilizando procesamiento de imágenes y video.
+
+## 🚀 Funcionalidades
+
+* 📷 Análisis en tiempo real usando cámara
+* 🎥 Análisis de postura a partir de un video
+* 📐 Cálculo de ángulos del cuerpo
+
+---
+
+## ▶️ Cómo ejecutar
+
+##    Activar env
+venv\Scripts\activate
+##    Desactivar env
+deactivate
+
+### 🔴 Tiempo real (cámara)
+
+```bash
+python Vista.py
+```
+
+### 🎬 Video
+
+```bash
+python Postura.py
+```
+
+> ⚠️ Importante: en el modo video, especificar la ruta del archivo dentro del código.
+
+---
+
+## 🧰 Requisitos
+
+* Python 3.11
+  
+Si se tiene otra version de python y la queres mantener, crea un entorno virtual
+con python 3.11 (si o si hay que instalarlo) y ejecuta este comando:
+
+* py -3.11 -m venv venv
+
+Instalar dependencias:
+
+```bash
+pip install -r requirements.txt
+```
+
+---
+
+## 📦 Instalación
+
+```bash
+git clone https://github.com/joaquinrios-unnoba/Imagenes-AngulosPostura.git
+cd Imagenes-AngulosPostura
+python -m venv venv
+venv\Scripts\activate   # En Windows
+pip install -r requirements.txt
+```
+
+---
+
+## 📁 Estructura
+
+* `Postura.py` → análisis desde video
+* `PosturaFrame.py` → análisis en tiempo real
+* `PosturaAngulo.py` → cálculo de ángulos
+* `Vista.py` → manejo de visualización
+
+---
+
+## ⚠️ Notas
+
+* No se incluye el entorno virtual (`venv`)
+* Asegurarse de usar Python 3.11 para evitar incompatibilidades (especialmente con MediaPipe)
+
+---
+
+## 👤 Autor
+
+Joaquin Rios
